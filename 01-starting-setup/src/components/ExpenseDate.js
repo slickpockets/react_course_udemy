125d78e4524524c9a8fee8e4eedd0e56e9afddf7
slickpockets <me@slickpockets.site>
2021-04-24 20:37:31 +0800

    section 37

--- 01-starting-setup/src/components/ExpenseDate.js
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -1,14 +1,15 @@
-function ExpenseDate() {
+function ExpenseDate(props) {
   const day =  props.date.toLocaleString("en-US", {day: '2-digit'});
   const month = props.date.toLocaleString("en-US", {month: 'long'});
   const year = props.date.getFullYear();
-  return (
+  return(
+    <div>
     <div>{day} </div>
     <div>{month} </div>
     <div>{year} </div>
-  )
-
-}
+  </div>
+)
+};
 
 
 export default ExpenseDate;
