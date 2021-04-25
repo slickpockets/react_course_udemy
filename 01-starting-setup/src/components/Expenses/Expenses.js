bd62a4b6c9e1c86c2f6abfa81a28911d6d10e006
slickpockets <slickpocketsroberts@gmail.com>
2021-04-26 03:23:33 +0800

    #49

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,5 +1,6 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 
 function Expenses(props) {
   const expenses = [
@@ -24,7 +25,7 @@ function Expenses(props) {
     },
   ];
   return (
-    <div>
+    <div className="expenses">
     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
     <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
     <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
