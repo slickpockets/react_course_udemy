f17d5e401c6bbeca7cf7da87d022141ac2545ef5
slickpockets <me@slickpockets.site>
2021-04-26 21:44:49 +0800

    .gitignore

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -3,13 +3,18 @@ import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
-  
+
+  const addExpenseHandler = expense => {
+    console.log('in app.js');
+    console.log(expense);
+  }
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
 
-      <Expenses />
+      <Expenses  />
     </div>
   );
 }
