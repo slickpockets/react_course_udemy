afd42e7043cf322905d29044f775a1d7f4c907d4
slickpockets <me@slickpockets.site>
2021-04-30 15:30:27 +0800

    new project boilerplate

--- 01-starting-setup/src/components/Chart/Chart.js
+++ 01-starting-setup/src/components/Chart/Chart.js
@@ -8,14 +8,14 @@ const Chart = (props) => {
   const totalMaximum = Math.max(...dataPointValues);
 
   return <div className ="chart">
-  {props.dataPoints.map(dataPoint =>
+  {props.dataPoints.map((dataPoint =>
     <ChartBar
     key={dataPoint.label}
      value={dataPoint.value}
       maxValue={totalMaximum}
        label={dataPoint.label}
        />
-     )}
+   ))}
   </div>
 };
 
