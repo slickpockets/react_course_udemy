8bec2072ebeb92d70337ba921b9e861d7c7cd2e9
slickpockets <me@slickpockets.site>
2021-06-07 22:38:41 +0800

    3/11

---
+++ section_8/01-starting-project/src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Card.module.css';
+
+const Card = (props) => {
+  return <div className={`${classes.card} ${props.classNamey}`}>{props.children}</div>;
+};
+
+export default Card;
