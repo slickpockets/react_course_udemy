e57a0f32400bd549cc0ba15ef40593a41f911d34
slickpockets <me@slickpockets.site>
2021-06-09 01:13:27 +0800

    validation

--- section_8/01-starting-project/src/components/Users/AddUser.js
+++ section_8/01-starting-project/src/components/Users/AddUser.js
@@ -10,7 +10,16 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+    {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredAge('');
+    setEnteredUsername('');
   };
 
   const usernameChangedHandler = (event) => {
@@ -25,9 +34,9 @@ const AddUser = (props) => {
     <Card classNamey={classes.input}>
       <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
-      <input id="username" type="text" onChange={usernameChangedHandler}/>
+      <input id="username" type="text" onChange={usernameChangedHandler} value={enteredUsername}/>
       <label htmlFor="age">Age (years)</label>
-      <input id="age" type="number" onChange={ageChangedHandler}/>
+      <input id="age" type="number" onChange={ageChangedHandler} value={enteredAge}/>
       <Button type="submit">Add User </Button>
       </form>
     </Card>
