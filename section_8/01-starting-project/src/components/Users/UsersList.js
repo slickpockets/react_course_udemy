ed45fc090ada55d9ef526e9d333fd3b35918fc01
slickpockets <me@slickpockets.site>
2021-06-09 14:58:40 +0800

    managing user state

--- section_8/01-starting-project/src/components/Users/UsersList.js
+++ section_8/01-starting-project/src/components/Users/UsersList.js
@@ -9,7 +9,7 @@ const UsersList = (props) => {
     <Card classNamey={classes.users}>
     <ul>
     {props.users.map(
-      user => <li>{user.name} ({user.age} years old)</li>
+      user => <li key={user.id}>{user.name} ({user.age} years old)</li>
     )}
      </ul>
      </Card>
