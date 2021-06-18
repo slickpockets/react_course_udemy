24712b9a5cc7d19b8fbf1c1e280d090433d4b116
slickpockets <me@slickpockets.site>
2021-06-19 05:05:51 +0800

    portals

--- section9/src/App.js
+++ section9/src/App.js
@@ -16,10 +16,10 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <React.Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </Fragment>
+    </React.Fragment>
   );
 }
 
