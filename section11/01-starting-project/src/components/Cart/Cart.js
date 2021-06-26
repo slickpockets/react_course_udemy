082db5ca4de0ecbb2330bd08d831daec9c4b95a7
slickpockets <slickpocketsroberts@gmail.com>
2021-06-26 22:09:15 +0800

    cart

--- section11/01-starting-project/src/components/Cart/Cart.js
+++ section11/01-starting-project/src/components/Cart/Cart.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import classes from './Cart.module.css';
-const Cart = (props) => {
+import CartItem from './CartItem';
 
+const Cart = (props) => {
+  const cartItems = <ul className={classes['cart-items']}>
+  [{id: 'c1', name: 'sushi', amount: 2, price: 12.99}].map((item) => (
+    <li> {item.name}</li>
+  ))}
+   </ul>
+ };
   return (
-    <button>{props.children}</button>
+    <div>
+    {cartItems}
+     <div className={classes.total}>
+     <span>Total Amount</span>
+     <span>35.61</span>
+        </div>
+     <div className={classes.actions}>
+     <button className={classes['button--alt']}>Close </button>
+     <button className={classes.button}>Order</button>
+     </div>
+     </div>
+
   )
 };
 
---
+++ section11/01-starting-project/src/components/Cart/CartItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CartItem = (props) => {
+
+};
+
+export default CartItem;
