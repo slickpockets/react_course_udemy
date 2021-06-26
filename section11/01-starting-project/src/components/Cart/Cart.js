825dabd6e13401a4d74a36e9dfe930c4abbb6565
slickpockets <slickpocketsroberts@gmail.com>
2021-06-26 22:31:53 +0800

    portal

--- section11/01-starting-project/src/components/Cart/Cart.js
+++ section11/01-starting-project/src/components/Cart/Cart.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
+import Modal from '../UI/Modal';
 
 const Cart = (props) => {
-  const cartItems = <ul className={classes['cart-items']}>
-  [{id: 'c1', name: 'sushi', amount: 2, price: 12.99}].map((item) => (
-    <li> {item.name}</li>
-  ))}
-   </ul>
- };
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
   return (
-    <div>
+    <Modal>
     {cartItems}
      <div className={classes.total}>
      <span>Total Amount</span>
@@ -20,7 +23,7 @@ const Cart = (props) => {
      <button className={classes['button--alt']}>Close </button>
      <button className={classes.button}>Order</button>
      </div>
-     </div>
+     </Modal>
 
   )
 };
