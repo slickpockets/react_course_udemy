2b6a1646c91d0e57756fcaa573566923202674dd
slickpockets <slickpocketsroberts@gmail.com>
2021-07-03 18:01:58 +0800

    ref and forward ref

--- section11/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
+++ section11/01-starting-project/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5)
+    {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
     <Input
+    ref={amountInputRef}
      label="Amount" input={{
       id: 'amount_' + props.id,
       type: 'number',
@@ -15,6 +34,7 @@ const MealItemForm = (props) => {
       defaultValue: '1'
     }} />
     <button>+ Add</button>
+    {!amountIsValid && <p> please enter a valid amont (1-5)</p>}
     </form>
   );
 };
