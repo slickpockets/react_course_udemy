bb80843e04fe87f479655cddc09f80de5b1a2f3c
slickpockets <slickpocketsroberts@gmail.com>
2021-07-03 22:23:02 +0800

    done

--- section11/01-starting-project/src/components/Layout/HeaderCartButton.js
+++ section11/01-starting-project/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import {useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from  '../../store/cart-context';
@@ -6,13 +6,16 @@ import CartContext from  '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${classes.bump}`;
+  useEffect(() => {
+
+  }, []);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
     <CartIcon />
     </span>
