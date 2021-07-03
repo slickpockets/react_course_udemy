2d76d28e2e35cb847459e84caa836adef7b96652
slickpockets <slickpocketsroberts@gmail.com>
2021-07-03 17:48:44 +0800

    useReducer

--- section11/01-starting-project/src/App.js
+++ section11/01-starting-project/src/App.js
@@ -3,7 +3,7 @@ import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import CartProvider from './store/cart-provider';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
