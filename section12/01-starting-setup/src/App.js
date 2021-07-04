b5629af323ed15ded6b80776707e1d1faffd1835
slickpockets <slickpocketsroberts@gmail.com>
2021-07-04 17:30:58 +0800

    use callback and depends

--- section12/01-starting-setup/src/App.js
+++ section12/01-starting-setup/src/App.js
@@ -5,19 +5,27 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }, []);
+    if (allowToggle){
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
 
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow toggle</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
