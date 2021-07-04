8e6adfa7a6904ab592174897e3abcdaafc95228b
slickpockets <slickpocketsroberts@gmail.com>
2021-07-04 14:25:57 +0800

    demo

--- section12/01-starting-setup/src/App.js
+++ section12/01-starting-setup/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -14,7 +15,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p> this is new </p>}
+      <DemoOutput show={showParagraph}/>
       <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
