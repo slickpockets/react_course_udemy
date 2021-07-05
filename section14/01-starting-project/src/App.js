7f9d99462ecdc9ce73cae5a37b883a879fab5b35
slickpockets <slickpocketsroberts@gmail.com>
2021-07-05 13:38:51 +0800

    async await

--- section14/01-starting-project/src/App.js
+++ section14/01-starting-project/src/App.js
@@ -6,11 +6,10 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/').then((response) => {
-      return response.json();
-    }).then((data) => {
-      const transformedMovies = data.results.map(movieData =>{
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData =>{
         return {
           id: movieData.episode_id,
           title: movieData.title,
@@ -19,8 +18,8 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-    });
-  }
+    };
+  
 
   return (
     <React.Fragment>
