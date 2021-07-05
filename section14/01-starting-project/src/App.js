5f02b718064de765f86ebe1cd1dead14b0f1e76b
slickpockets <slickpocketsroberts@gmail.com>
2021-07-05 14:24:11 +0800

    move to 06

--- section14/01-starting-project/src/App.js
+++ section14/01-starting-project/src/App.js
@@ -16,7 +16,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-e6e00-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error(error.message);
       }
