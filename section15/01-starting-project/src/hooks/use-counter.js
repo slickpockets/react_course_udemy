a1f194b158cd5230c8c37613ccf8a1b5c4101253
slickpockets <slickpocketsroberts@gmail.com>
2021-07-06 11:40:45 +0800

    backwards

--- section15/01-starting-project/src/hooks/use-counter.js
+++ section15/01-starting-project/src/hooks/use-counter.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const useCounter = (step) => {
+const useCounter = (step = 1) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
@@ -9,7 +9,7 @@ const useCounter = (step) => {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [step]);
 
   return counter;
 };
