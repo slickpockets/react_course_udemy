ffe37e408e7ed861ce06e09866477615f32caba4
slickpockets <slickpocketsroberts@gmail.com>
2021-07-07 17:55:43 +0800

    error validitate

--- section16/src/components/SimpleInput.js
+++ section16/src/components/SimpleInput.js
@@ -4,6 +4,7 @@ import { useRef, useState } from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameValid, setEnteredNameValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,17 +13,22 @@ const SimpleInput = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredName.trim() === ''){
+      setEnteredNameValid(false);
       return;
     }
+    setEnteredNameValid(true);
     console.log(nameInputRef.current.value);
     setEnteredName('');
   };
+
+  const nameInputClasses = enteredNameValid ? 'form-control' : 'form-control invalid';
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
       </div>
+      {!enteredNameValid && <p className="error-text"> name cant be empty</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
